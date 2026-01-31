0d5aadd6fc8ab936edbe24f69c9039a715e7460f
Embersong <evebay@mail.ru>
2026-01-31 22:30:01 +0700

    Db added

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 use DI\Container;
+use Slim\Flash\Messages;
 use Slim\Views\PhpRenderer;
 
 $container = new Container();
@@ -11,12 +12,32 @@ $container->set('renderer', function () {
     return new PhpRenderer(__DIR__ . '/../templates');
 });
 
+$container->set('flash', function () {
+    return new Messages();
+});
+
+$container->set(\PDO::class, function () {
+    $databaseUrl = parse_url(getenv('DATABASE_URL'));
+    $username = $databaseUrl['user'];
+    $password = $databaseUrl['pass'];
+    $host = $databaseUrl['host'];
+    $dbName = ltrim($databaseUrl['path'], '/');
+
+    $conn = new \PDO("pgsql:dbname=$dbName;host=$host", $username, $password);
+    $conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    return $conn;
+});
+
+$initFilePath = implode('/', [dirname(__DIR__), 'database.sql']);
+$initSql = file_get_contents($initFilePath);
+$container->get(\PDO::class)->exec($initSql);
+
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml', []);
-
 });
+
 $app->run();
